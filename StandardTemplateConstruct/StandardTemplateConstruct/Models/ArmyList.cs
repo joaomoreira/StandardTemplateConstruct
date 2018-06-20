62b3c845e903437f4dd13cae0d344e1e43ee7e22
João Moreira <jpmoreira@agap2.pt>
2018-06-20 11:15:25 +0200

    Models

---
+++ StandardTemplateConstruct/StandardTemplateConstruct/Models/ArmyList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StandardTemplateConstruct.Models
+{
+    public class ArmyList
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public IList<Unit> Units { get; set; }
+    }
+}
---
+++ StandardTemplateConstruct/StandardTemplateConstruct/Models/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StandardTemplateConstruct.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public UnitType UnitType { get; set; }
+    }
+}
